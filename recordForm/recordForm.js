604db6e2cc3b9fc9e0627f1dcc18c673441ea5c8
xu.jiuwu <xu.jiuwu@cn.nssol.nipponsteel.com>
2021-02-22 18:55:48 +0800

    first commit v0.01

---
+++ recordForm/recordForm.js
@@ -0,0 +1,52 @@
+import { LightningElement, track, api} from 'lwc';
+
+export default class RecordForm extends LightningElement {
+    @api recordId
+    @api sections
+    @track editMode = false
+    get allsections() {
+        try {
+        const all = []
+        this.sections.forEach(section => {
+            all.push(section.id)
+        })
+        return all
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    onLoad() {
+        console.log(this.sections)
+    }
+
+    handleSuccess() {
+
+    }
+
+    // rerenderedCallBack
+    renderedCallback() {
+        // make section header has a gray background color and a porper height & padding
+        const style = document.createElement('style');
+        style.innerText = `.slds-accordion__summary-action {
+        background: rgb(238, 241, 246);
+        height: 2rem;
+        padding: 0 0 0 0.5rem;
+    }`
+        // make output-field has a border-bottom line (same as standard compoent)
+        this.template.querySelector('.text') && this.template.querySelector('.text').appendChild(style);
+        const style_output = document.createElement('style');
+        style_output.innerText = `
+        .field-output > lightning-output-field > .slds-form-element__control{
+            border-bottom: 1px solid rgb(221, 219, 218)
+    }
+    }`
+        // make section part without border-top line.
+        this.template.querySelector('lightning-accordion') && this.template.querySelector('lightning-accordion').appendChild(style_output);
+        const style_topline = document.createElement('style');
+        style_topline.innerText = `.slds-accordion__list-item {
+        border-top: none;
+    }`
+        this.template.querySelector('lightning-accordion') && this.template.querySelector('lightning-accordion').appendChild(style_topline);
+    }
+}
